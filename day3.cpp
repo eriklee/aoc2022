162e1e106f1f0aeda8922219b97321697bba5a72
Erik Lee <eriklee113@gmail.com>
2022-12-04 15:03:13 +0000

    day3: branchless version of priority

--- day3.cpp
+++ day3.cpp
@@ -9,6 +9,16 @@ uint64_t priority(char c) {
   return 1ul << ((c < 'a') ? (c - 'A' + 27) : (c - 'a' + 1));
 }
 
+// alternative branchless version
+// only actually branchless after optimizations
+// day3 bench is ~4x faster with optimizations
+uint64_t priority2(char c) {
+  int isLC = c & ('a' - 'A');
+  int sh = (c - 'A');
+  sh += isLC ? (1 - ('a' - 'A')) : 27;
+  return 1ul << sh;
+}
+
 int priorityToNum(uint64_t p) {
   uint64_t res = 0;
   while (p != 0) {
@@ -22,7 +32,7 @@ int priorityToNum(uint64_t p) {
 uint64_t catalogueItems(std::string_view s) {
   uint64_t has = 0;
   for (const auto c : s) {
-    auto prio = priority(c);
+    auto prio = priority2(c);
     has |= prio;
   }
   return has;
@@ -37,11 +47,22 @@ uint64_t findSharedPriority(const std::string &s) {
 }
 
 int main(int argc, char **argv) {
+
+  for (int i = 0; i < 26; i++) {
+    assert(priority('A' + i) == priority2('A' + i));
+    assert(priority('a' + i) == priority2('a' + i));
+  }
+
   assert(priorityToNum(priority('a')) == 1);
   assert(priorityToNum(priority('z')) == 26);
   assert(priorityToNum(priority('A')) == 27);
   assert(priorityToNum(priority('P')) == 42);
   assert(priorityToNum(priority('Z')) == 52);
+  assert(priorityToNum(priority2('a')) == 1);
+  assert(priorityToNum(priority2('z')) == 26);
+  assert(priorityToNum(priority2('A')) == 27);
+  assert(priorityToNum(priority2('P')) == 42);
+  assert(priorityToNum(priority2('Z')) == 52);
 
   std::string l;
   {
