52ce408a354ab41ac0749ae5f4f80cd68ea52a8c
Erik Lee <eriklee113@gmail.com>
2022-12-04 16:56:55 +0000

    day3_bench: fixup a bit

--- aoc.sh
+++ aoc.sh
@@ -2,4 +2,5 @@
 ./day1 # compiled with `ghc -O day1`
 ./day2 # compiled with `ghc -O day2`
 ./day3 # compiled with `g++ -g -o day3 -Wall -O3 --std=c++20 day3.cpp`
-# ./day3_bench # compiled with `g++ -g -o day3_bench -Wall -O3 --std=c++20 day3_bench.cpp`
+./day3_bench # compiled with `g++ -g -o day3_bench -Wall -O3 --std=c++20 day3_bench.cpp`
+./day3 # compiled with `g++ -g -o day3 -Wall -O3 --std=c++20 -lfmt day4.cpp`
