7b70b407c215387215d936cc3e4f1a816b19f12f
Erik Lee <eriklee113@gmail.com>
2022-12-07 23:43:49 +0000

    Big news, clangd no longer has a million errors in neovim

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
   name = "aoc-2022";
   src = ./.;
 
-  buildInputs = [ ghc gcc_latest fmt_9 gdb clang_14 clang-tools_14 ];
-
+  # argh. so clang must go after clang-tools or clangd does not work in vim
+  # Thanks random reddit comment flake that pointed this out...
+  buildInputs = [ ghc fmt_9 gdb clang-tools_14 clang_14 bear ];
 }
