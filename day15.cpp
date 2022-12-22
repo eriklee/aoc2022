0c761a7058a481f3fcbfd80758f4f7bd00e57d8b
Erik Lee <eriklee113@gmail.com>
2022-12-22 01:55:37 +0000

    day15 pt2
    this is probably not the fastest way to do it...

--- day15.cpp
+++ day15.cpp
@@ -1,13 +1,13 @@
 #include <cassert>
-#include <tuple>
-#include <optional>
 #include <charconv>
 #include <deque>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 #include <fstream>
 #include <functional>
+#include <optional>
 #include <string>
+#include <tuple>
 #include <vector>
 
 struct LineInfo {
@@ -39,22 +39,26 @@ LineInfo parseLine(std::string_view sv) {
 }
 
 void test_parse() {
- auto l = "Sensor at x=2881987, y=1923522: closest beacon is at x=2528182, y=2000000";
- auto li = parseLine(l);
- assert(li.sat_x == 2881987);
- assert(li.sat_y == 1923522);
- assert(li.beacon_x == 2528182);
- assert(li.beacon_y == 2000000);
+  auto l = "Sensor at x=2881987, y=1923522: closest beacon is at x=2528182, "
+           "y=2000000";
+  auto li = parseLine(l);
+  assert(li.sat_x == 2881987);
+  assert(li.sat_y == 1923522);
+  assert(li.beacon_x == 2528182);
+  assert(li.beacon_y == 2000000);
 }
 
-std::optional<std::pair<int, int>> getCover(const int64_t lineIndex, const LineInfo& li) {
-  auto dist = std::abs(li.sat_x - li.beacon_x) + std::abs(li.sat_y - li.beacon_y);
+std::optional<std::pair<int64_t, int64_t>> getCover(const int64_t lineIndex,
+                                            const LineInfo &li) {
+  auto dist =
+      std::abs(li.sat_x - li.beacon_x) + std::abs(li.sat_y - li.beacon_y);
 
   int64_t dist_to_line = std::abs(li.sat_y - lineIndex);
-  if (dist_to_line > dist) return std::nullopt;
+  if (dist_to_line > dist)
+    return std::nullopt;
 
   auto rem = dist - dist_to_line;
-  return {{ li.sat_x - rem, li.sat_x + rem }};
+  return {{li.sat_x - rem, li.sat_x + rem}};
 }
 
 void test_getCover() {
@@ -65,49 +69,88 @@ void test_getCover() {
   assert(cover->second == 14);
 }
 
-bool tryMerge(std::pair<int, int>& a, const std::pair<int, int>& b) {
+bool tryMerge(std::pair<int64_t, int64_t> &a, const std::pair<int64_t, int64_t> &b) {
   assert(a.first <= b.first); // they don't overlap
-  if (b.first > a.second) return false;
+  if (b.first > a.second + 1)
+    return false;
   a.second = std::max(a.second, b.second); // (ax... bx.. [ay...by])
   return true;
 }
 
-
 int main(int argc, char **argv) {
+  {
+    // std::ifstream file("inp/day15-test.txt");
+    std::ifstream file("inp/day15.txt");
+    std::string l;
+
+    test_parse();
+    test_getCover();
+
+    std::vector<std::pair<int64_t, int64_t>> line_overlaps;
+
+    static constexpr int64_t lineIndex = 2000000;
+    // static constexpr int64_t lineIndex = 10;
+
+    while (std::getline(file, l)) {
+      auto ov = getCover(lineIndex, parseLine(l));
+      // fmt::print("{}: \n", l);
+      if (ov) {
+        line_overlaps.push_back(*ov);
+        fmt::print("({} -> {})\n", ov->first, ov->second);
+      }
+    }
+    std::sort(line_overlaps.begin(), line_overlaps.end(),
+              [](const auto a, const auto b) { return a.first < b.first; });
+    auto it = line_overlaps.begin();
+    while (it != line_overlaps.end()) {
+      while (it + 1 != line_overlaps.end() && tryMerge(*it, *(it + 1))) {
+        fmt::print("merging {} and {}\n", *it, *(it + 1));
+        line_overlaps.erase(it + 1);
+      }
+      it++;
+    }
+    // we should be left with only non-overlapping areas now, so just add up the
+    // sizes
+    int res1 = 0;
+    for (const auto &[x, y] : line_overlaps) {
+      res1 += (y - x);
+    }
+    fmt::print("Day15: Part1: {}\n", res1);
+  }
+
   // std::ifstream file("inp/day15-test.txt");
   std::ifstream file("inp/day15.txt");
   std::string l;
 
-  test_parse();
-  test_getCover();
+  std::vector<LineInfo> lis;
 
-  std::vector<std::pair<int, int>> line_overlaps;
-
-  static constexpr int64_t lineIndex = 2000000;
-  // static constexpr int64_t lineIndex = 10;
-
+  // static constexpr int maxSz = 20;
+  static constexpr int64_t maxSz = 4000000;
   while (std::getline(file, l)) {
-    auto ov = getCover(lineIndex, parseLine(l));
-    // fmt::print("{}: \n", l);
-    if (ov) {
-      line_overlaps.push_back(*ov);
-      fmt::print("({} -> {})\n", ov->first, ov->second);
-    }
+    lis.push_back(parseLine(l));
   }
-  std::sort(line_overlaps.begin(), line_overlaps.end(), [](const auto a, const auto b) { return a.first < b.first; });
-  auto it = line_overlaps.begin();
-  while (it != line_overlaps.end()) {
-    while (it + 1 != line_overlaps.end() && tryMerge(*it, *(it + 1))) {
-      fmt::print("merging {} and {}\n", *it, *(it + 1));
-      line_overlaps.erase(it + 1);
-    }
-    it++;
-  }
-  // we should be left with only non-overlapping areas now, so just add up the sizes
-  int res1 = 0;
-  for (const auto& [x,y] : line_overlaps) {
-    res1 += (y - x);
-  }
-  fmt::print("Day15: Part1: {}\n", res1);
-}
 
+  std::vector<std::pair<int64_t, int64_t>> line_overlaps;
+
+  for (int64_t y = 0; y < maxSz; y++) {
+    line_overlaps.clear();
+    for (const auto &li : lis) {
+      auto ov = getCover(y, li);
+      if (ov)
+        line_overlaps.push_back(*ov);
+    }
+    std::sort(line_overlaps.begin(), line_overlaps.end(),
+              [](const auto a, const auto b) { return a.first < b.first; });
+    auto it = line_overlaps.begin();
+    while (it != line_overlaps.end()) {
+      while (it + 1 != line_overlaps.end() && tryMerge(*it, *(it + 1))) {
+        line_overlaps.erase(it + 1);
+      }
+      it++;
+    }
+    if (line_overlaps.size() >= 2) {
+      // fmt::print("non-0 overlaps: {}: {}\n", x, fmt::join(line_overlaps, ","));
+      fmt::print("Day15: Part2: {}\n", maxSz * (line_overlaps[0].second + 1) + y);
+    }
+  }
+}
